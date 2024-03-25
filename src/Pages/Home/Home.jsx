f4290ad3ea4725945821ec58f668b0689fbcd5c9
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-03-26 03:30:13 +0600

    start a assaignment 8

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../../Components/Nav/Nav";
+
+const Home = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <Nav></Nav>
+            <h1>This is home page</h1>
+        </div>
+    );
+};
+
+export default Home;
