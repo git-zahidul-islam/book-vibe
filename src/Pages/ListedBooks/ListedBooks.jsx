6056c2a67730203d2ed72e45c6f2e9e65dc256ff
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-03-26 19:07:17 +0600

    listed page design start here

--- src/Pages/ListedBooks/ListedBooks.jsx
+++ src/Pages/ListedBooks/ListedBooks.jsx
@@ -1,7 +1,18 @@
+import { RiArrowDropDownLine } from "react-icons/ri";
+
 const ListedBooks = () => {
     return (
         <div>
-            <h1 className="text-2xl">Listed book added here</h1>
+            <h1 className="text-xl text-center font-bold text-[#131313] bg-[#1313130D] py-8 rounded-2xl">Books</h1>
+            <div className="flex justify-center">
+                <div className="dropdown dropdown-hover mt-5">
+                    <div tabIndex={0} role="button" className="btn m-1 text-lg bg-[#23BE0A] text-white">Sort By<RiArrowDropDownLine /></div>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -3,7 +3,7 @@ import Nav from "../../Components/Nav/Nav";
 
 const Root = () => {
     return (
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto mb-8">
             <Nav></Nav>
             <Outlet></Outlet>
         </div>
