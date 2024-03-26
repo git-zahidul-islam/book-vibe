e4681c9240c71fd6f777da0ace15a4d4f0d6874f
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-03-26 09:18:11 +0600

    home page complete

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 const Banner = ({ bannerContent }) => {
     return (
@@ -6,17 +6,16 @@ const Banner = ({ bannerContent }) => {
             <div className="flex justify-around items-center">
                 <div className="space-y-5">
                     <h1 className="text-5xl font-bold w-[500px] leading-[70px]">{bannerContent.bookName}</h1>
-                    <div className="flex"><button className="btn-primary btn">All Show Book</button></div>
+                    <div className="flex"><button className="bg-[#23BE0A] btn text-white text-xl">All Show Book</button></div>
                 </div>
-                <img className="w-80" src={bannerContent.images} alt="" />
+                <img className="w-80" src={bannerContent.images} alt="image" />
             </div>
-
         </div>
     );
 };
 
 Banner.propTypes = {
-
+    bannerContent: PropTypes.object.isRequired
 };
 
 export default Banner;
--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,24 +1,36 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { FaRegStar } from "react-icons/fa";
 
 const Books = ({ books }) => {
-    // const { tags } = books
+    const { tags, bookName, author, category, rating, images } = books
     return (
         <div>
             <div className="p-6 rounded-md shadow-md border-2">
-                <img src="https://source.unsplash.com/random/300x300/?1" alt="" className="object-cover object-center w-full rounded-md h-72 bg-gray-500" />
-                <div className="mt-6 mb-2">
-                    <span className="block text-xs font-medium tracking-widest uppercase
-                     text-violet-400">hhh</span>
-                    <h2 className="text-xl font-semibold tracking-wide">Nam maximus purus</h2>
+                <div className="rounded-md h-72 flex items-center justify-center bg-[#F3F3F3]">
+                    <img src={images} alt="image" />
                 </div>
-                <p className="text-gray-100">Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.</p>
+                <div className="mt-6 mb-2 space-y-3">
+                    <div className="flex gap-3">
+                        <span className="block text-base font-medium tracking-widest uppercase
+                     text-[#23BE0A] bg-[#23BE0A0D] px-4 p-2">{tags[0]}</span>
+                        <span className="block text-base font-medium tracking-widest uppercase
+                     text-[#23BE0A] bg-[#23BE0A0D] px-4 py-2">{tags[1]}</span>
+                    </div>
+                    <h2 className="text-2xl font-bold text-[#131313] tracking-wide">{bookName}</h2>
+                    <p className="text-base font-medium text-[#131313CC]">By : {author}</p>
+                </div>
+                <hr className="border-[1px] border-dashed text-[#13131326] mt-4" />
+                <div className="flex justify-between mt-4">
+                    <p className="text-base font-medium text-[#131313CC]">{category}</p>
+                    <p className="text-base font-medium text-[#131313CC] flex gap-2 items-center">{rating} <FaRegStar /></p>
+                </div>
             </div>
         </div>
     );
 };
 
 Books.propTypes = {
-
+    books: PropTypes.object.isRequired
 };
 
 export default Books;
