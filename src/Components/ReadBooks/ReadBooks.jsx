eb8c8a9cc75cc535272c5c90417982310d29b062
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-03-28 11:41:42 +0600

    some change sort button

--- src/Components/ReadBooks/ReadBooks.jsx
+++ src/Components/ReadBooks/ReadBooks.jsx
@@ -1,4 +1,5 @@
 // import PropTypes from 'prop-types';
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 import { useEffect, useState } from "react";
 import { getRead } from "../../Utilities/getData";
@@ -14,10 +15,21 @@ const ReadBooks = () => {
 
 
     return (
-        <div className="mt-5 space-y-5">
+        <div className="space-y-5">
+            <div className="dropdown dropdown-hover absolute lg:left-[640px] left-[250px] top-52">
+                <div tabIndex={0} role="button" className="btn m-1 text-lg bg-[#23BE0A] text-white">Sort By<RiArrowDropDownLine /></div>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
+                    <li><a>Rating</a></li>
+                    <li><a>Number Of Page</a></li>
+                    <li><a>Publish Of Year</a></li>
+                </ul>
+            </div>
+
             {
                 read.map(read => <ReadCard key={read.bookId} read={read}></ReadCard>)
             }
+
+
         </div>
     );
 };
--- src/Pages/ListedBooks/ListedBooks.jsx
+++ src/Pages/ListedBooks/ListedBooks.jsx
@@ -1,37 +1,29 @@
 import { useState } from "react";
-import { RiArrowDropDownLine } from "react-icons/ri";
+// import { RiArrowDropDownLine } from "react-icons/ri";
 import { Link, Outlet } from "react-router-dom";
 
 const ListedBooks = () => {
-    const [tabIndex,setTabIndex] = useState(0)
-    
-    
+    const [tabIndex, setTabIndex] = useState(0)
+
+
     return (
-        <div>
-            <h1 className="text-xl text-center font-bold text-[#131313] bg-[#1313130D] py-8 rounded-2xl">Books</h1>
-            <div className="flex justify-center">
-                <div className="dropdown dropdown-hover mt-5">
-                    <div tabIndex={0} role="button" className="btn m-1 text-lg bg-[#23BE0A] text-white">Sort By<RiArrowDropDownLine /></div>
-                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 2</a></li>
-                    </ul>
+        <div className="">
+            <h1 className="text-xl text-center font-bold text-[#131313] bg-[#1313130D] py-8 rounded-2xl mt-5">Books</h1>
+            <div className="mt-24">
+                <div className="flex items-center sm:justify-start overflow-x-auto overflow-y-hidden flex-nowrap">
+                    <Link
+                        onClick={() => setTabIndex(0)}
+                        to='' className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-400 text-gray-400`}>
+                        <span>Read Books</span>
+                    </Link>
+                    <Link
+                        onClick={() => setTabIndex(1)}
+                        to={`wishlistBooks`} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-400 text-gray-500`}>
+                        <span>Wishlist Books</span>
+                    </Link>
                 </div>
             </div>
-            {/* tab items */}
-            <div className="flex items-center sm:justify-start overflow-x-auto overflow-y-hidden flex-nowrap">
-                <Link
-                    onClick={() => setTabIndex(0)}
-                    to='' className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0' : 'border-b' } rounded-t-lg border-gray-400 text-gray-400`}>
-                    <span>Read Books</span>
-                </Link>
-                <Link
-                    onClick={() => setTabIndex(1)}
-                    to={`wishlistBooks`} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b' } rounded-t-lg border-gray-400 text-gray-500`}>
-                    <span>Wishlist Books</span>
-                </Link>
-            </div>
-            
+
             {/* outlet here */}
             <Outlet></Outlet>
         </div>
