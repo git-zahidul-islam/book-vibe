33e572cb59749f3757dc232bdf94940d768a363d
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-03-28 21:09:38 +0600

    1 this is problem & all complete

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -6,7 +6,10 @@ const ErrorPage = () => {
         <div className="flex flex-col justify-center items-center h-screen space-y-3">
             <h1 className="text-4xl font-bold">Oops!</h1>
             <p className="text-xl font-medium">Sorry, an unexpected error has occurred.</p>
-            <p className="text-xl"> {error.statusText || error.message} </p>
+            <p className="text-5xl font-medium text-red-500">404</p>
+            <p className="text-xl"> {
+                error.statusText || error.message
+            } </p>
             <Link to={'/'}><button className="btn btn-error mt-4 text-white text-xl">Back To Home</button></Link>
         </div>
     );
