5dae31c880691682ce41796080a093e7e4290d9d
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-03-28 06:44:19 +0600

    custome chart added

--- src/Pages/PageToRead/PageToRead.jsx
+++ src/Pages/PageToRead/PageToRead.jsx
@@ -1,8 +1,56 @@
 
+import { useEffect, useState } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { getRead } from '../../Utilities/getData';
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+
+// other data
+const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+};
+
+const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+
 const PageToRead = () => {
+    const [readBook, setReadBook] = useState([])
+    console.log(readBook)
+
+
+    useEffect(() => {
+        const loadData = getRead()
+        setReadBook(loadData)
+    }, [])
+
     return (
-        <div>
-            <h1 className="text-2xl">Page to read page here</h1>
+        <div className='bg-[#13131308] h-[calc(100vh-125px)] mt-4 flex justify-center items-center rounded-xl'>
+            <BarChart
+                width={1270}
+                height={500}
+                data={readBook}
+                margin={{
+                    top: 20,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey={'bookName'} />
+                <YAxis />
+                <Bar dataKey={'totalPages'} fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                    {readBook.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                    ))}
+                </Bar>
+            </BarChart>
         </div>
     );
 };
